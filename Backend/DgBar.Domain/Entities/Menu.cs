5d619dbaffbdc18f66a5245d7e91db94c4cdfa91
Thalita Mariano <8.litah.8@gmail.com>
2020-08-16 12:39:14 -0300

    adding entities

---
+++ Backend/DgBar.Domain/Entities/Menu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DgBar.Domain.Entities
+{
+    public class Menu
+    {
+        public int Id { get; set; }
+        public string Item { get; set; }
+        public decimal? Price { get; set; }
+    }
+}
---
+++ Backend/DgBar.Domain/Entities/SheetOrder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace DgBar.Domain.Entities
+{
+    public class SheetOrder
+    {
+        public BigInteger Id { get; set; }
+        public decimal? TotalPrice { get; set; }
+        public decimal? TotalDiscount { get; set; }
+
+
+        public SheetOrder()
+        {
+            //  var random = Guid.NewGuid();
+            // Id = new BigInteger(random.ToByteArray());
+        }
+
+    }
+}
